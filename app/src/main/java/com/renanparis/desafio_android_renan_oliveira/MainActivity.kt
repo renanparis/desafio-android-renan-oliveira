258bba02a0bf10653a12ac67ff86a14e921626c1
renanparis <paris.oliveira@gmail.com>
2020-08-10 19:54:48 -0300

    Criação do projeto

---
+++ app/src/main/java/com/renanparis/desafio_android_renan_oliveira/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.renanparis.desafio_android_renan_oliveira
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
