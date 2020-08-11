277fefdc692de7b931fa05a956b87f45c9469c8d
renanparis <paris.oliveira@gmail.com>
2020-08-10 23:31:45 -0300

    Adiciona layout List Characters

--- app/src/main/java/com/renanparis/desafio_android_renan_oliveira/ui/activity/ListCharactersActivity.kt
+++ app/src/main/java/com/renanparis/desafio_android_renan_oliveira/ui/activity/ListCharactersActivity.kt
@@ -1,13 +1,13 @@
-package com.renanparis.desafio_android_renan_oliveira.ui
+package com.renanparis.desafio_android_renan_oliveira.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.renanparis.desafio_android_renan_oliveira.R
 
-class MainActivity : AppCompatActivity() {
+class ListCharactersActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_list_characters)
     }
 }
