325f07a037916dfacf3a75df09ffb05d0a58df0b
renanparis <paris.oliveira@gmail.com>
2020-08-11 22:49:50 -0300

    Adiciona Listener os itens da lista de characters

--- app/src/main/java/com/renanparis/desafio_android_renan_oliveira/ui/adapter/ListCharactersAdapter.kt
+++ app/src/main/java/com/renanparis/desafio_android_renan_oliveira/ui/adapter/ListCharactersAdapter.kt
@@ -11,7 +11,8 @@ import com.renanparis.desafio_android_renan_oliveira.data.model.Character
 import com.renanparis.desafio_android_renan_oliveira.extensions.setImage
 import kotlinx.android.synthetic.main.item_list_characters.view.*
 
-class ListCharactersAdapter : PagedListAdapter<Character, ListCharactersAdapter.ViewHolder>(diff) {
+class ListCharactersAdapter (var onItemClickListener: (character: Character?) -> Unit = {}) :
+    PagedListAdapter<Character, ListCharactersAdapter.ViewHolder>(diff) {
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -20,33 +21,35 @@ class ListCharactersAdapter : PagedListAdapter<Character, ListCharactersAdapter.
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_list_characters, parent, false)
         return ViewHolder(view)
+
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val character = getItem(position)
         holder.bind(character)
+        holder.itemView.setOnClickListener {
+            onItemClickListener(character)
+        }
     }
 
 
     class ViewHolder(item: View) : RecyclerView.ViewHolder(item) {
-        private lateinit var character: Character
         private val nameText = item.item_name_character
         private val image = item.item_image_character
 
 
         fun bind(character: Character?) {
-            character?.let { this.character = it }
-            nameText.text = this.character.name
+            nameText.text = character?.name
             image.setImage("${character?.thumbnail?.path}/standard_medium.${character?.thumbnail?.extension}")
         }
-
     }
 
     companion object {
-        val diff = object: DiffUtil.ItemCallback<Character>() {
+        val diff = object : DiffUtil.ItemCallback<Character>() {
             override fun areItemsTheSame(oldItem: Character, newItem: Character): Boolean {
                 return oldItem.id == newItem.id
             }
+
             override fun areContentsTheSame(oldItem: Character, newItem: Character): Boolean {
                 return oldItem == newItem
             }
