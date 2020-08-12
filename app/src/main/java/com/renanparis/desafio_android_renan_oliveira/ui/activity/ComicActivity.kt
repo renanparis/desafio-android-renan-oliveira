68c6edb979c2ac64398f6c73dc536dd2edae2c1a
renanparis <paris.oliveira@gmail.com>
2020-08-12 20:00:44 -0300

    Layout Comic Activity

---
+++ app/src/main/java/com/renanparis/desafio_android_renan_oliveira/ui/activity/ComicActivity.kt
@@ -0,0 +1,13 @@
+package com.renanparis.desafio_android_renan_oliveira.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.renanparis.desafio_android_renan_oliveira.R
+
+class ComicActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_comic)
+    }
+}
